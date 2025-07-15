0b7709f4d0d76e0fc07bce0b720a5b6422c41548
DanikMehanik <117514457+DanikMehanik@users.noreply.github.com>
2025-07-15 16:24:20 +0300

    Delete Sirius_GazpromNeft-main/Sirius_GazpromNeft-main directory

--- Sirius_GazpromNeft-main/Sirius_GazpromNeft-main/well-plan-optimization-main/src/wellplan/data/base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from wellplan.core import Well
-
-class BaseDataLoader(ABC):
-    @abstractmethod
-    def load(self) -> list[Well]:
-        pass
--- Sirius_GazpromNeft-main/Sirius_GazpromNeft-main/well-plan-optimization-main/src/wellplan/services/__init__.py
+++
@@ -1,7 +0,0 @@
-from .cost import NPV, BaseCapex, BaseOpex
-from .team_manager import TeamManager, DistanceTeamMovement, SimpleTeamMovement
-from .production import LinearProductionProfile, FileProductionProfile, ArpsDeclineProductionProfile
-from .infrastructure import SimpleInfrastructure
-from .risk_strategy import ClusterRandomRiskStrategy
-from .constraint import OilConstraint, CapexConstraint
-
